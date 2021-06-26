8e64692b28aa623e1570e9bdd6a5a8b2386ca291
thaovy29 <thaovyduong2902@gmail.com>
2021-06-26 11:01:31 +0700

    Fix indent 4, use Laravel Mix

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LanguageController extends Controller
+{
+    public function index(Request $request, $language){
+        if ($language){
+            $request->session()->put('language', $language);
+        }
+        
+        return redirect()->back();
+    }
+}
