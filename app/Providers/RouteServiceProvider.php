3042685f377701bf6d0d765dab97c80bb9a796b4
phatntt1999 <phatntt1999@gmail.com>
2021-07-13 10:21:33 +0700

    authorization handling

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const ADMINHOME = '/admin/tours';
+    public const USERHOME = '/';
 
     /**
      * The controller namespace for the application.
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\ListTourController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('language/{language}', [LanguageController::class, 'index'])->name('language');
@@ -13,7 +14,7 @@ Route::get('/destinations', function () {
 })->name('destinations');
 
 Route::get('/tour', function () {
-    return view('tour');
+    return view('welcome');
 })->name('tour');
 
 Route::get('/dashboard', function () {
@@ -21,3 +22,5 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__ . '/auth.php';
+
+Route::resource('admin/tours', ListTourController::class);
